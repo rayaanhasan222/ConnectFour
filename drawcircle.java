4ab5d64ea3f2c352d2dcecb7ce3e2ce195e9280e
Rayaan Hasan <62673140+rayaanhasan222@users.noreply.github.com>
2022-03-23 15:28:58 -0500

    Add files via upload

---
+++ drawcircle.java
@@ -0,0 +1,21 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class drawcircle extends Canvas
+{
+
+	public drawcircle()
+	{	
+	}
+	public void paint( Graphics window ) {
+		window.setColor(Color.WHITE);
+		int x1 = 10;
+		int y1=10;
+		for(int x = 10; x<=810;x+=100) {
+			for(int y = 10; y<=810;y+=100) {
+				window.drawOval(x,y,90,90);
+			}
+		}
+	}
+}
